c9b11a891c59458bd78f4352ff4533a751559d9f
mvladislav <dev@mvladislav.online>
2023-11-17 03:56:43 +0100

    test: dynamic set var (PEP 646)

--- custom_components/deebot/select.py
+++ custom_components/deebot/select.py
@@ -3,7 +3,7 @@ from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Any, Generic
 
-from deebot_client.capabilities import CapabilitySetTypes
+from deebot_client.capabilities import CapabilitySetTypes, CapabilityTypes
 from deebot_client.device import Device
 from homeassistant.components.select import SelectEntity, SelectEntityDescription
 from homeassistant.config_entries import ConfigEntry
@@ -21,7 +21,7 @@ class DeebotSelectEntityMixin(Generic[EventT]):
     """Deebot select entity mixin."""
 
     current_option_fn: Callable[[EventT], str | None]
-    options_fn: Callable[[CapabilitySetTypes], list[str]]
+    options_fn: Callable[[CapabilityTypes], list[str]]
 
 
 @dataclass
@@ -34,18 +34,6 @@ class DeebotSelectEntityDescription(
 
 
 ENTITY_DESCRIPTIONS: tuple[DeebotSelectEntityDescription, ...] = (
-    DeebotSelectEntityDescription(
-        capability_fn=lambda caps: caps.clean.auto_empty
-        if caps.clean.auto_empty
-        else None,
-        current_option_fn=lambda e: e.mode.display_name,
-        options_fn=lambda aut: [mode.display_name for mode in aut.types],
-        key="auto_empty",
-        translation_key="auto_empty",
-        entity_registry_enabled_default=False,
-        icon="mdi:delete-empty",
-        entity_category=EntityCategory.CONFIG,
-    ),
     DeebotSelectEntityDescription(
         capability_fn=lambda caps: caps.settings.efficiency_mode,
         current_option_fn=lambda e: e.efficiency.display_name,
@@ -88,6 +76,21 @@ ENTITY_DESCRIPTIONS: tuple[DeebotSelectEntityDescription, ...] = (
     ),
 )
 
+ENTITY_ENABLE_DESCRIPTIONS: tuple[DeebotSelectEntityDescription, ...] = (
+    DeebotSelectEntityDescription(
+        capability_fn=lambda caps: caps.clean.auto_empty
+        if caps.clean.auto_empty
+        else None,
+        current_option_fn=lambda e: e.mode.display_name,
+        options_fn=lambda aut: [mode.display_name for mode in aut.types],
+        key="auto_empty",
+        translation_key="auto_empty",
+        entity_registry_enabled_default=False,
+        icon="mdi:delete-empty",
+        entity_category=EntityCategory.CONFIG,
+    ),
+)
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -99,10 +102,13 @@ async def async_setup_entry(
     controller.register_platform_add_entities(
         DeebotSelectEntity, ENTITY_DESCRIPTIONS, async_add_entities
     )
+    controller.register_platform_add_entities(
+        DeebotSelectEnableEntity, ENTITY_ENABLE_DESCRIPTIONS, async_add_entities
+    )
 
 
 class DeebotSelectEntity(
-    DeebotEntity[CapabilitySetTypes[EventT, str], DeebotSelectEntityDescription],
+    DeebotEntity[CapabilitySetTypes[EventT, str, str], DeebotSelectEntityDescription],
     SelectEntity,  # type: ignore
 ):
     """Deebot select entity."""
@@ -112,7 +118,7 @@ class DeebotSelectEntity(
     def __init__(
         self,
         device: Device,
-        capability: CapabilitySetTypes[EventT, str],
+        capability: CapabilitySetTypes[EventT, str, str],
         entity_description: DeebotSelectEntityDescription | None = None,
         **kwargs: Any,
     ):
@@ -134,3 +140,40 @@ class DeebotSelectEntity(
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
         await self._device.execute_command(self._capability.set(option))
+
+
+class DeebotSelectEnableEntity(
+    DeebotEntity[
+        CapabilitySetTypes[EventT, bool, str, str], DeebotSelectEntityDescription
+    ],
+    SelectEntity,  # type: ignore
+):
+    """Deebot select entity."""
+
+    _attr_current_option: str | None = None
+
+    def __init__(
+        self,
+        device: Device,
+        capability: CapabilitySetTypes[EventT, bool, str, str],
+        entity_description: DeebotSelectEntityDescription | None = None,
+        **kwargs: Any,
+    ):
+        super().__init__(device, capability, entity_description, **kwargs)
+        self._attr_options = self.entity_description.options_fn(capability)
+
+    async def async_added_to_hass(self) -> None:
+        """Set up the event listeners now that hass is ready."""
+        await super().async_added_to_hass()
+
+        async def on_water_info(event: EventT) -> None:
+            self._attr_current_option = self.entity_description.current_option_fn(event)
+            self.async_write_ha_state()
+
+        self.async_on_remove(
+            self._device.events.subscribe(self._capability.event, on_water_info)
+        )
+
+    async def async_select_option(self, option: str) -> None:
+        """Change the selected option."""
+        await self._device.execute_command(self._capability.set(True, option))
